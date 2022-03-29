c36cff073fc844553ac713f8eb8b43ca2ae2abcc
kokomi simp :3 <beol0127@gmail.com>
2022-03-29 15:43:49 +0000

    chore: update styling

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const path = require('path');
 
 
+app.use(express.static(__dirname + '/public'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.enable('verbose errors');
