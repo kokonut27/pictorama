8a42c1976d7cba451d8155fcf591f9dbb515de93
kokomi simp :3 <beol0127@gmail.com>
2022-03-29 15:34:20 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+// canvas app with drawing tools - can also post like instagram
+const express = require('express');
+const app = express();
+
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.enable('verbose errors');
+
+
+app.get('/', (req, res) => {
+  res.render('index.ejs');
+});
+
+app.get('/404', (req, res, next) => {
+  next();
+});
+
+app.use((req, res, next) => {
+  res.status(404);
+
+  res.format({
+    html: function () {
+      res.render('404', { url: req.url })
+    },
+    json: function () {
+      res.json({ error: 'Not found' })
+    },
+    default: function () {
+      res.type('txt').send('Not found')
+    }
+  })
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.render('500', { error: err });
+});
+
+app.listen(8080, () => {
+  console.log('drawvan is running');
+});
